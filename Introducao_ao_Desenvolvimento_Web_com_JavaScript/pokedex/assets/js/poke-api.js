aa550968f63039cf7c2753b9657c381173f27490
monica.marcal <monicagmarcal@outlook.com>
2023-09-07 17:03:03 -0300

    feat:pegando os detalhes dos pokemons

--- Introducao_ao_Desenvolvimento_Web_com_JavaScript/pokedex/assets/js/poke-api.js
+++ Introducao_ao_Desenvolvimento_Web_com_JavaScript/pokedex/assets/js/poke-api.js
@@ -1,10 +1,17 @@
 const pokeApi = {}
 
+pokeApi.getPokemonsDetail = (pokemon) => {
+    return fetch(pokemon.url) //solicitando a url de detalhes dos pokemons
+        .then((response) => response.json())
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
-        .catch((error) => console.error(error))
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonsDetail))//transformando a lista de pokemon em lista dos detalhes dele
+        .then((detailRequests) => Promise.all(detailRequests))//requsições dos detalhes do pokemon, esperando a solicitação terminar
+        .then((pokemonDetails) => pokemonDetails)//quando terminada a solicitação mostra lista dos detalhes
 }
