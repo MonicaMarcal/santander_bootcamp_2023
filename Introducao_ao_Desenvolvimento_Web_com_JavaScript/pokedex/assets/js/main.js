1d2e85a9473e57af13a4c4caef03b6874ca70a0e
monica.marcal <monicagmarcal@outlook.com>
2023-09-08 16:24:37 -0300

    feat: adicionando nosso modelo de pokemon

--- Introducao_ao_Desenvolvimento_Web_com_JavaScript/pokedex/assets/js/main.js
+++ Introducao_ao_Desenvolvimento_Web_com_JavaScript/pokedex/assets/js/main.js
@@ -7,17 +7,20 @@ function convertPokemonTypesToLi(pokemonTypes){
 
 
 function convertPokemonToLi(pokemon){
-    return `<li class="pokemon">
-    <!-- ..... Pokemons here ..... -->
-    <span class="number">${pokemon.order}</span>
-    <span class="name">${pokemon.name}</span>
-    <div class="detail">
-        <ol class="types">
-           ${convertPokemonTypesToLi(pokemon.types).join('')}
-        </ol>
-        <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
-    </div>
-    </li>`
+    return `<li class="pokemon ${pokemon.type}">
+        <span class="number">#${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
+
+        <div class="detail">
+            <ol class="types">
+                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+            </ol>
+
+            <img src="${pokemon.photo}"
+                 alt="${pokemon.name}">
+        </div>
+    </li>
+`
 }
 
 const pokemonList = document.getElementById('pokemonList')
---
+++ Introducao_ao_Desenvolvimento_Web_com_JavaScript/pokedex/assets/js/pokemon-model.js
@@ -0,0 +1,7 @@
+class Pokemon {
+    number;
+    name;
+    type; //tipo principal
+    types = []; //varios tipos
+    photo;
+}
