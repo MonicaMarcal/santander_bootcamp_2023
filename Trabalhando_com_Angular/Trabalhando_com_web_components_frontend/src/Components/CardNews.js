30cfb01c6f7fba30a8b618ab62da846d15eac460
monica.marcal <monicagmarcal@outlook.com>
2023-09-13 15:35:37 -0300

    feat: criando os elementos de div e setando propriedades de classe

--- Trabalhando_com_Angular/Trabalhando_com_web_components_frontend/src/Components/CardNews.js
+++ Trabalhando_com_Angular/Trabalhando_com_web_components_frontend/src/Components/CardNews.js
@@ -3,10 +3,27 @@ class Cardnews extends HTMLElement{
         super();
 
     const shadow = this.attachShadow({ mode: "open" });
-    shadow.innerHTML= "<h1>hello word</h1>"
+    shadow.innerHTML= "<h1>hello word</h1>" //cria sombra(shadow dom)
+    shadow.appendChild(this.build()); //adiciona todos os elementos HTML
+    shadow.appendChild(this.styles());//adiciona todos os estilos css
     }
     
-    build(){}
+    build(){
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");
+
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class", "card__left");
+
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class", "card__right");
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+
+
+        return componentRoot
+    }
 
     styles(){}
 }
