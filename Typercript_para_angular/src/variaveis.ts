6e3baf5ad90879480952a87e4141098758db4e83
monica.marcal <monicagmarcal@outlook.com>
2023-09-14 15:19:37 -0300

    docs:(variaveis): trabalhando com variaveis

---
+++ Typercript_para_angular/src/variaveis.ts
@@ -0,0 +1,17 @@
+/**
+ * VARIAVEIS
+ */
+
+//tipos primitivos: boolean, number, string
+let ligado: boolean = false;
+let nome: string = "felipe";
+let idade: number = 30;    //inteiro
+let altura: number = 1.9;  //decimal
+
+// tipos especiais null, undefined
+let nulo: null = null;                  //um valor vazio ou um valor em branco.
+let indefinido: undefined = undefined;  //variável foi declarada, mas seu valor não foi atribuído
+
+//tipos abrangentes: any, void
+let retorno: void;            //void = vazio, não retorna nada
+let retornoView: any = false; //aceita qualquer coisa
