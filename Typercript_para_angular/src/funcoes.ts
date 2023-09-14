9b45a488335901e89a8bbb4e49f1ba5a9635a2dc
monica.marcal <monicagmarcal@outlook.com>
2023-09-14 15:42:55 -0300

    docs:(funções)Funções Multi Tipos

--- Typercript_para_angular/src/funcoes.ts
+++ Typercript_para_angular/src/funcoes.ts
@@ -8,7 +8,11 @@ function addToHello(name: string): string {
     return `Hello ${name}`;
   }
 
+function CallToPhone(phone: number | string): number | string { //aceita tanto numero quanto texto
+    return phone;
+}
+  
 
 let soma: number = addNumber(4, 7);
-
 console.log(addToHello("OLA"));
+console.log(CallToPhone(123456));
