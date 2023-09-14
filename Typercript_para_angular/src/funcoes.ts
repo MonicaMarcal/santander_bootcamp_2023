bad212009749868c8aca2b4367c00b4db69fe3c1
monica.marcal <monicagmarcal@outlook.com>
2023-09-14 15:39:20 -0300

    docs:(funções)Trabalhando com funções

---
+++ Typercript_para_angular/src/funcoes.ts
@@ -0,0 +1,14 @@
+//FUNÇÕES
+
+function addNumber(x: number, y: number): number {
+    return x + y;
+}
+
+function addToHello(name: string): string {
+    return `Hello ${name}`;
+  }
+
+
+let soma: number = addNumber(4, 7);
+
+console.log(addToHello("OLA"));
