f618756843d334ba6915d45fc04e014d884c89de
monica.marcal <monicagmarcal@outlook.com>
2023-09-14 16:09:40 -0300

    docs: trabalhando com as classes nas interfaces

--- Typercript_para_angular/src/interfaces.ts
+++ Typercript_para_angular/src/interfaces.ts
@@ -11,6 +11,7 @@ type robot = {
   interface robot2 {  
     readonly id: number | string;
     name: string;
+    sayHello(): string;
   }
   
   const bot1: robot = { 
@@ -18,10 +19,30 @@ type robot = {
     name: "megaman",
   };
   
-  const bot2: robot2 = { 
+  const bot2: robot2 = {
     id: "1",
     name: "megaman",
-}
+    sayHello: function (): string {
+      throw new Error("Function not implemented.");
+    },
+  };
+  
 
 console.log(bot1);
 console.log(bot2);
+
+class Pessoa implements robot2 {
+    id: string | number;
+    name: string;
+  
+    constructor(id: string | number, name: string) { //toda vez que a interface for criada passa por esse metodo aqui
+      this.id = id;
+      this.name = name;
+    }
+    sayHello(): string {
+      return `hello i'm ${this.name}`;
+    }
+  }
+  
+  const p = new Pessoa(1, "gutsman");
+  console.log(p.sayHello());
