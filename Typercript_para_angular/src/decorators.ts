6359bb7dec8a201c53ca965d473ab4236e475108
monica.marcal <monicagmarcal@outlook.com>
2023-09-15 13:47:53 -0300

    docs: Atrribute Decorator

--- Typercript_para_angular/src/decorators.ts
+++ Typercript_para_angular/src/decorators.ts
@@ -1,9 +1,10 @@
 //DECORATORS -> decore uma função para que dispare uma ação, quando essa função decorator aparecer.
 //Ex: toda vez que você ir ao mercado e ver o cafe com valor menos de 10 reais, compre ele.
 
+//_____________________________________________________________________________//
 //temos varia APIs e queremos documentar as versoes dessas APIs:
 //class decorator
-function apiVersion(version: string) {
+/* function apiVersion(version: string) {
     return (target: any) => { //função que retorna outra função
       Object.assign(target.prototype, { __version: version, __name: "monica" });
     };
@@ -13,4 +14,42 @@ function apiVersion(version: string) {
 class Api{}
 
 const api = new Api()
-console.log(api.__name) //rodar com o start:dev, pois os decorator ainda não são bem entendidos pelo typescript, fazendo assim não passa pelo build
+console.log(api.__name) //rodar com o start:dev, pois os decorator ainda não são bem entendidos pelo typescript, fazendo assim não passa pelo build */
+
+//_____________________________________________________________________________//
+
+//attribute decorator
+function minLength(length: number) {
+    return (target: any, key: string) => {
+      let _value = target[key];
+  
+      const getter = () => "[play]" + _value; //metodo de leitura -get-
+      const setter = (value: string) => {//metodo de setar valor -set-
+        if (value.length < length) {
+          throw new Error(`Tamanho menor do que ${length}`);
+        } else {
+          _value = value;
+        }
+      };
+  
+      Object.defineProperty(target, key, {
+        get: getter,
+        set: setter,
+      });
+    };
+  }
+  
+  class Api {
+    //@minLength(5) //setando decorator com tamanho de 5 strings
+    @minLength(10) //setando decorator com tamanho de 10 strings
+    name: string;
+  
+    constructor(name: string) {
+      this.name = name;
+    }
+  }
+  
+  //const api = new Api("usa"); //não passs,pois não há setado o tamanho de três strings
+  //const api = new Api("usaada"); //passa
+  const api = new Api("usaadasdaaaaa"); //passa
+  console.log(api.name);
