a2999a86b1f8aff652700dca2bd934dca00fb29b
monica.marcal <monicagmarcal@outlook.com>
2023-09-15 13:33:32 -0300

    docs: usando decorator em class

--- Typercript_para_angular/src/decorators.ts
+++ Typercript_para_angular/src/decorators.ts
@@ -1,12 +1,16 @@
 //DECORATORS -> decore uma função para que dispare uma ação, quando essa função decorator aparecer.
 //Ex: toda vez que você ir ao mercado e ver o cafe com valor menos de 10 reais, compre ele.
 
-function exibirNome(target: any){
-    console.log(target)
+//temos varia APIs e queremos documentar as versoes dessas APIs:
+//class decorator
+function apiVersion(version: string) {
+    return (target: any) => { //função que retorna outra função
+      Object.assign(target.prototype, { __version: version, __name: "monica" });
+    };
 }
 
-@exibirNome // chamando decorator 
-class Funcionario{}
+@apiVersion("1.10")
+class Api{}
 
-@exibirNome // chamando decorator
-class Dono{}
+const api = new Api()
+console.log(api.__name) //rodar com o start:dev, pois os decorator ainda não são bem entendidos pelo typescript, fazendo assim não passa pelo build
