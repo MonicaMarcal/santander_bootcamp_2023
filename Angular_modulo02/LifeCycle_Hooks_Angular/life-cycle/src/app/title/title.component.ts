d261bfb9097fc4ba305e5f8eab29f8b4b7f179a2
monica.marcal <monicagmarcal@outlook.com>
2023-09-25 22:18:07 -0300

    feat: adicionando o evento OnInit - ciclo inicial de vida

--- Angular_modulo02/LifeCycle_Hooks_Angular/life-cycle/src/app/title/title.component.ts
+++ Angular_modulo02/LifeCycle_Hooks_Angular/life-cycle/src/app/title/title.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
-export class TitleComponent {
+export class TitleComponent implements OnInit {
+
+   //criando uma propriedade
+   nome:string = 'Monica'
+
+   constructor(){}
+
+   //evento a ser disparado
+   ngOnInit(): void {
+     this.nome = `Olá ${this.nome}`
+     console.log("Olá eu sou o nascimento do componente title")
+   }
 
 }
