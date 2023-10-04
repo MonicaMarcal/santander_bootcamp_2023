079ff5d4213d85e6c3c33aaebb215ab687b72de3
monica.marcal <monicagmarcal@outlook.com>
2023-10-04 14:26:40 -0300

    feat: service conectado ao com environment

--- Angular_modulo03/Trabalhando_com_Services_no_Angular/services/src/app/services/pokemon.service.ts
+++ Angular_modulo03/Trabalhando_com_Services_no_Angular/services/src/app/services/pokemon.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core'; //conteudo que pode ser injetado em outros componentes
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root' //pasta root do projeto
 })
 export class PokemonService {
+  private baseURL:string = ""
 
-  constructor( ) { }
+  constructor( ) {
+    this.baseURL  = environment.pokeApi
+   }
 
   getPokemon(pokemonName:string) {
     console.log(pokemonName)
