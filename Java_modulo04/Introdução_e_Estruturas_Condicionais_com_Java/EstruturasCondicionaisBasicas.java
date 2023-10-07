dc9234a60f03688c264b6dbada33337ea723bd6b
monica.marcal <monicagmarcal@outlook.com>
2023-10-07 16:12:49 -0300

    docs: Introdução e Estruturas Condicionais com Java

---
+++ Java_modulo04/Introdução_e_Estruturas_Condicionais_com_Java/EstruturasCondicionaisBasicas.java
@@ -0,0 +1,5 @@
+public class EstruturasCondicionaisBasicas {
+    public static void main(String[] args) {
+        System.out.println("Documentação sobre Introdução e Estruturas Condicionais com Java");
+    }
+}
