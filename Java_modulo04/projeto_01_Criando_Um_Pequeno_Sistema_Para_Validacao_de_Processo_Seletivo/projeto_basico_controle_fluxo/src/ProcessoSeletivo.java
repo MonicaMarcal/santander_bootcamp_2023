c1b641d4ac8f0d95ee64f5c3c069bffef2839641
monica.marcal <monicagmarcal@outlook.com>
2023-10-07 21:15:56 -0300

    feat: case2 - selecionando candidatos

--- Java_modulo04/projeto_01_Criando_Um_Pequeno_Sistema_Para_Validacao_de_Processo_Seletivo/projeto_basico_controle_fluxo/src/ProcessoSeletivo.java
+++ Java_modulo04/projeto_01_Criando_Um_Pequeno_Sistema_Para_Validacao_de_Processo_Seletivo/projeto_basico_controle_fluxo/src/ProcessoSeletivo.java
@@ -1,10 +1,33 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        selecaoCandidatos();
     }
 
+    static void selecaoCandidatos(){
+        // Array com a lista de candidatos
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
+
+        int candidatosSelecionados = 0;
+        int candidatosAtuais = 0;
+        double salarioBase = 2000.0;
+        while (candidatosSelecionados < 5 && candidatosAtuais < candidatos.length ){
+            String candidato = candidatos[candidatosAtuais];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("o candidato" + candidato + "solicitou este valor de salario" + salarioPretendido);
+            if(salarioBase >= salarioPretendido){
+                System.out.println("O candidato" + candidato + "foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatosAtuais++;
+        }
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
     static void analisarCandidato(double salarioPretendido) {
         double salarioBase = 2000.0;
         if (salarioBase > salarioPretendido) {
